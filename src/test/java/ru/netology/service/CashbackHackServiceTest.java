02c6324473ec99ff5e890f52236e1bd2b1ef2556
iva1111 <mvg75@mail.ru>
2021-09-20 12:30:02 +0500

    test branch testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+        @Test
+        public void shouldAddAmountUp1000 () {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 900;
+            int expected = 100;
+            int actual = service.remain(amount);
+            assertEquals(actual, expected);
+        }
+
+        @Test
+        public void shouldReturn10IfAmountIs2000() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 2000;
+            int expected = 10;
+            int actual = service.remain(amount);
+            assertEquals(actual, expected);
+        }
+
+        @Test
+        public void shouldCalculateBonusFor1000() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 1000;
+            int expected = 0;
+            int actual = service.remain(amount);
+            assertEquals(actual, expected);
+        }
+
+}
